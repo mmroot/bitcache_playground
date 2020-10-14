58f67d2a554210945b286d5e9c817074d4ccaf40
mmroot <razvan.petre@gmail.com>
2020-10-14 17:12:05 +0200

    added handcash.js

--- handcash.js
+++ handcash.js
@@ -4,9 +4,9 @@ const query = {
   "v": 3,
   "q": {
     "find": {
-      //"out.s1": "1PqRhzVabqJqnvjNy8nb9CD3amT8NkRmJH",
-      "out.s2": "16ncN7NF8zXrvhFtW7LXqqLRdzoFDsaH9m",
-      //"out.f3": { $exists: true }
+      "out.s1": "1PqRhzVabqJqnvjNy8nb9CD3amT8NkRmJH",
+      //"out.s2": "16ncN7NF8zXrvhFtW7LXqqLRdzoFDsaH9m",
+      "out.f3": { $exists: true }
     },
     "project": { "out.f3": 1, "tx.h": 1 }
     //"limit": 200
@@ -28,7 +28,7 @@ bitcache.init({
     query: query,
   },
   bus: {
-    from: 650000,
+    from: 656849,
     url: "https://txo.bitbus.network",
     token: "eyJhbGciOiJFUzI1NksiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiIxOExuejRRVllZSEo3dlJraDFydDRIMTZyWE1oc3pZMUg0IiwiaXNzdWVyIjoiZ2VuZXJpYy1iaXRhdXRoIn0.SUphUXJaTWdZTGVOZDFmb1Q2c09xSlozbnlUa2JWbW8zeXNVTnBPYU12djNhNEFFZjUrUVpBQ1Q2MUpZbUk5bW50eUNwQUtaNmcrbkhXOFppenhLd2hzPQ",
     query: query,
